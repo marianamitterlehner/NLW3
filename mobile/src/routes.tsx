ef04b105a00abef3a80186508fffd69813a71b38
Mariana Miguel <mariana.m.miguel@gmail.com>
2020-10-17 23:42:24 -0300

    MobileV3

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -13,7 +13,7 @@ import SelectMapPosition from './pages/CreateOrphanages/SelectMapPosition';
 export default function Routes(){
     return(
         <NavigationContainer>
-            <Navigator screenOptions={{headerShown:false}}>
+            <Navigator screenOptions={{headerShown:false, cardStyle:{ backgroundColor:'#f2f3f5'} }}>
                 <Screen name="OrphanagesMap" component={OrphanagesMap}/>
                 <Screen name="OrphanagesDetails" component={OrphanagesDetails}/>
                 <Screen name="OrphanagesData" component={OrphanagesData}/>
